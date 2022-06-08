b137ce95be6427fcb30182c8b99d49d7de46af7e
sarigsela <sarig.sela@gmail.com>
2022-06-08 10:00:27 +0300

    defining the main

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 import csv
 from DataModel import *
 
-def readFile(fileNamePath, mySite):
+def readFile(fileNamePath, inventory):
         f = open(fileNamePath, "r")
         i = 0
         for row in f:
             if i > 10:
                 break
             i += 1
-            print(row)
+            #print(row)
             if i > 1:
-                populateRow2Site(row, mySite)
+                populateRow2Site(row, inventory)
 
-def populateRow2Site(row, mySite: Site):
+def populateRow2Site(row, inventory):
     row_arr = row.split(',')
     area_id = row_arr[1]
     locus_id = row_arr[2]
     basket_id = row_arr[4]
+    # TBD add more attributes
 
-    # TBD - isaac/yulia to complete
-    # ...
-    if area_id in mySite.areas:
-        area = mySite.areas[area_id]
-    else:
-        area = Area(area_id, area_id)
-        mySite.areas[area_id] = area
+    site = Site('DOR', 'DOR')
+    area = Area(area_id, area_id)
+    locus = Locus(locus_id, locus_id)
+    basket = Basket(basket_id, site, area, locus)
+    inventory[basket_id] = basket
 
-    if locus_id in mySite.areas[area_id].loci:
-        locus = mySite.areas[area_id].loci[locus_id]
-    else:
-        locus = Locus(locus_id, locus_id)
-        mySite.areas[area_id].loci[locus_id] = locus
+def getBasket(basketId, inventory):
+    if basketId in inventory:
+        return inventory[basketId]
+    return None
 
-    if basket_id in mySite.areas[area_id].loci[locus_id].baskets:
-        basket = mySite.areas[area_id].loci[locus_id].baskets[basket_id]
-    else:
-        basket = Basket(basket_id, basket_id)
-        mySite.areas[area_id].loci[locus_id].baskets[basket_id] = basket
+def getbaskets(type, inventory):
+    baskets = []
+    for basket_id in inventory:
+        if inventory[basket_id].type == type:
+            baskets.append(inventory[basket_id])
+    return baskets
+
 
 if __name__ == '__main__':
-    mySite = Site('DOR', 'DOR')
-    readFile('data.csv', mySite)
-    print(mySite)
-    for basket in mySite.areas['D2'].loci['19736'].baskets:
-        print(basket)
+    INVENTORY = {}
+    readFile('data.csv', INVENTORY)
+    print(getBasket('195048', INVENTORY))
+    print(getbaskets('Grinding stone', INVENTORY))
     print('end')
